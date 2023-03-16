222cfa5744f39daee788cccb8d5d03d5f77536a0
hung <hunglt.ee@gmail.com>
2023-03-16 09:53:48 +0700

    ruby secure

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,8 +4,13 @@ class ArticlesController < ApplicationController
 
   end
   def create
-    @article = Article.all
-    puts "AAAAA #{@article.inspect}"
+    puts params[:article].class
+    # chuyển từ giá trị HASH sang OBJECT
+    @article = Article.new(params[:article])
+    # GIÁ TRỊ CỦA CÁC INSTANCE VẪN LÀ OBJECT
+    puts @article.class
+    @article.save
+    puts "Congra #{Article.all.inspect}"
     redirect_to root_path
   end
 end
