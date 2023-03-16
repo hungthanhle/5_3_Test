22463953edc3f38f074bf5ed7cdd950206537318
hung <hunglt.ee@gmail.com>
2023-03-16 15:41:12 +0700

    U

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,18 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
   private
     def article_params
       params.require(:article).permit(:email)
