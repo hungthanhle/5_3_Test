493a0c48c0223fe6917bc6905a9896f248859ffb
hung <hunglt.ee@gmail.com>
2023-03-21 16:12:23 +0700

    second model 1-N migrate route controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  has_many :comments
   validates :email, presence: true,
                     length: { minimum: 5 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_17_071018) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_085413) do
   create_table "articles", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -24,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_17_071018) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "articles"
 end
